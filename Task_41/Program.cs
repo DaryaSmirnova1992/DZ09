bdcb6ed284f58330ecce41697881a618444b6197
Darya <darecc@yandex.ru>
2022-09-08 19:03:41 +0400

    add Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите числа через запятую: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+int num = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        num++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во чисел > 0: {num}");
